4fac153fe23eab22040ecdf11aa1ff965652f358
antonio krijan <superdev0055@gmail.com>
2023-05-09 16:58:45 +0300

    backend final'

--- server.js
+++ server.js
@@ -3,39 +3,78 @@ const { writeFileSync } = require("fs");
 const bodyParser = require("body-parser");
 var app = express();
 var cors = require('cors')
-// var image = require("./image.json");
-// var image = require("./image(background).json");
-var image = require("./image1.json")
+var user_templates = require("./user_templates.json");
+var demo_templates = require("./demo_templates.json");
+// to write
+const path_user = "./user_templates.json";
+const path_demo = "./demo_templates.json";
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 
-app.get('/feed-image', function(req, res){
-   return res.json(image);
+app.get('/user/get-all-templates', function(req, res){
+   return res.json(user_templates);
 });
-app.get('/feed-image/:id', function(req, res){
-   return res.json(image['feed-image'][req.params.id]);
+
+app.get('/user/get-specific-template/:id', function(req, res){
+
+   var template = user_templates['user_templates'].filter((item)=>{
+      return item.template_id == req.params.id;
+   });
+   return res.json(template[0]);   
 });
-app.post('/feed-image/edit/:id', function(req, res){
-   image['feed-image'][req.params.id] = req.body.data;
-   const path = "./image.json";
+
+app.post('/user/update-template/:id', function(req, res){
+   var index = user_templates['user_templates'].findIndex((item)=>{
+      return item.template_id == req.params.id;
+   });   
+   user_templates['user_templates'][index] = req.body.data;
    try {
-       writeFileSync(path, JSON.stringify(image, null, 2), "utf8");
+
+       writeFileSync(path_user, JSON.stringify(user_templates, null, 2), "utf8");
+       return res.json("Data successfully saved");
+
+   } catch (error) {
+
+      return res.json("An error has occurred ", error);
+
+   }
+});
+
+app.post('/user/create-template', function(req, res){
+   user_templates['user_templates'].push(req.body.data);
+        
+   try {
+       writeFileSync(path_user, JSON.stringify(user_templates, null, 2), "utf8");
        return res.json("Data successfully saved");
    } catch (error) {
       return res.json("An error has occurred ", error);
    }
 });
-app.post('/feed-image/create', function(req, res){
-   console.log("create")
-   image1['feed-image'].push(req.body.data);
-   const path = "./image1.json";     
-   try {
-       writeFileSync(path, JSON.stringify(image, null, 2), "utf8");
-       return res.json("Data successfully saved");
-   } catch (error) {
-      return res.json("An error has occurred ", error);
-   }
+
+
+app.get("/demo/get-all-templates",function(req,res){
+   return res.json(demo_templates);
 });
+app.get("/demo/get-specific-template/:id",function(req,res){
+   console.log(req.params.id);
+   var template = demo_templates['demo_templates'].filter((item)=>{
+      return item.template_id == req.params.id;
+   });
+   return res.json(template[0]);
+});
+
+
+// app.post('/user/create-template', function(req, res){
+//    console.log("create");
+//    demo_templates['demo_templates'].push(req.body.data);
+        
+//    try {
+//        writeFileSync(path_demo, JSON.stringify(demo_templates, null, 2), "utf8");
+//        return res.json("Data successfully saved");
+//    } catch (error) {
+//       return res.json("An error has occurred ", error);
+//    }
+// });
 app.listen(3000);
